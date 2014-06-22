4a4daee27779d08eb851921b43264448bf55281d
Paulo McNally <paulomcnally@gmail.com>
2014-06-22 04:55:35 -0600

    remove tweets prog

--- controllers/home.js
+++ controllers/home.js
@@ -41,32 +41,11 @@ exports.index = function (req, res) {
                     callback(null, null);
                 }
             });
-        },
-        tweets: function (callback) {
-            var q = models.tweets.find({}).sort({'created_at': -1}).limit(10);
-            q.exec(function (err, rows) {
-                if (!err && rows) {
-                    callback(null, rows);
-                }
-                else {
-                    callback(null, null);
-                }
-            });
         }
     },
     function (err, results) {
 
-        var all_tweets = [];
-
-        if( results.tweets ){
-            results.tweets.forEach(function(tweet){
-               var new_tweet = tweet;
-                new_tweet.text = helpers.tweets.autoLink( tweet.text );
-                all_tweets.push(new_tweet);
-            });
-        }
-
-        res.render(helpers.site.template( 'index' ), { blog: results.blog, questions: results.questions, training: results.training, tweets: all_tweets });
+        res.render(helpers.site.template( 'index' ), { blog: results.blog, questions: results.questions, training: results.training });
 
     });
 
