nodenica/nodenica-website

commit 13fa7ba212805093ec49e30c0c43809dc6d90da9

Similar to d15d181, but handles cases where a Sentry log URL haven't been defined.
With this and d15d181 we allow logging system to gracefully degrade to old plain console.

helpers/log.js
@@ -1,20 +1,24 @@
 var config = require('../config');
 var raven = require('raven');
 
-var client = new raven.Client( config.log.url );
+var client = config.log.url ? new raven.Client( config.log.url ) : false;
 
 // Simple message
 exports.message = function( message, level ){
 
     level = level || 'info';
 
-    client.captureMessage( message, {level: level} );
+        client.captureMessage( message, {level: level} );
+        console.info(message);
 
 }
 
 // Error message
 exports.error = function( message ){
 
-    client.captureError(new Error( message ));
+    if (client)
+        client.captureError(new Error( message ));
+    else
+        console.error(message);
 
 }

commit 886709a59fa65abd34c01fd38dd6fa82b80eabcb

Fix syntax error introduced in 13fa7ba.

helpers/log.js
@@ -8,17 +8,23 @@ exports.message = function( message, level ){
 
     level = level || 'info';
 
+    if (client) {
         client.captureMessage( message, {level: level} );
+    }
+    else {
         console.info(message);
-
+    }
 }
 
 // Error message
 exports.error = function( message ){
 
-    if (client)
+    if (client) {
         client.captureError(new Error( message ));
-    else
+    }
+    else {
         console.error(message);
+    }
+
 
 }